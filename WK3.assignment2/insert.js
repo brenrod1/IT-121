34e377ad0783bfba614c4dbdd989076bba46755e
brenrod1 <brenrod1@gmail.com>
2023-04-23 14:17:33 -0700

    Add files via upload

---
+++ WK3.assignment2/insert.js
@@ -0,0 +1,14 @@
+// var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+// console.log(months);
+// do {
+//     var nowDate = Date();nowDate.setMonth(nowDate.getDate() +12);
+    
+//     nowDate = ("Date 12 months ahead is " + nowDate.months());
+//     ("lengthOfmonths " + "12" + [monthnowDate.get])
+//     let lengthOfmonths + 
+//     console.log("12 months from now.date");
+// } while (isNaN (nowDate ));
+
+const today = new Date();
+const nextYear = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate());
+console.log(new Date + nextYear);
